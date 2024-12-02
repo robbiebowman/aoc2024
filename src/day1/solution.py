ad0e2e483081a7eab36ebc4e3b670452a9c493a8
Robbie Bowman <robbiebowman@macbookpro.mynetworksettings.com>
2024-12-01 21:40:36 -0500

    Further refinement

--- src/day1/solution.py
+++ src/day1/solution.py
@@ -14,16 +14,11 @@ def part2(first_location_codes, second_location_codes):
     return sum(sorted_second.count(value) * value for value in sorted_first)
 
 
-def solve(first_location_codes, second_location_codes):
-    return part1(first_location_codes, second_location_codes), part2(
-        first_location_codes, second_location_codes
-    )
-
-
 if __name__ == "__main__":
     data = [line.split() for line in read_input(1)]
     first_location_codes = [int(x[0]) for x in data]
     second_location_codes = [int(x[1]) for x in data]
-    solution1, solution2 = solve(first_location_codes, second_location_codes)
-    print(f"Part 1: {solution1}") # 1110981
-    print(f"Part 2: {solution2}") # 24869388
+    solution1 = part1(first_location_codes, second_location_codes)
+    solution2 = part2(first_location_codes, second_location_codes)
+    print(f"Part 1: {solution1}")  # 1110981
+    print(f"Part 2: {solution2}")  # 24869388
