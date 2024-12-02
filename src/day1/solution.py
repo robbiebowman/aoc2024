bb3439bc96b69e3afd28a28c702c7780beecdeab
Robbie Bowman <robbiebowman@macbookpro.mynetworksettings.com>
2024-12-01 21:39:05 -0500

    Day One

--- src/day1/solution.py
+++ src/day1/solution.py
@@ -1,26 +1,29 @@
 from src.utils import read_input
 
-def parse_input(puzzle_input):
-    """Parse the puzzle input."""
-    return puzzle_input
 
-def part1(data):
-    """Solve part 1 of the puzzle."""
-    pass
+def part1(first_location_codes, second_location_codes):
+    return sum(
+        abs(x - y)
+        for x, y in zip(sorted(first_location_codes), sorted(second_location_codes))
+    )
 
-def part2(data):
-    """Solve part 2 of the puzzle."""
-    pass
 
-def solve(puzzle_input):
-    """Solve the puzzle for the given input."""
-    data = parse_input(puzzle_input)
-    solution1 = part1(data)
-    solution2 = part2(data)
-    return solution1, solution2
+def part2(first_location_codes, second_location_codes):
+    sorted_first = sorted(first_location_codes)
+    sorted_second = sorted(second_location_codes)
+    return sum(sorted_second.count(value) * value for value in sorted_first)
+
+
+def solve(first_location_codes, second_location_codes):
+    return part1(first_location_codes, second_location_codes), part2(
+        first_location_codes, second_location_codes
+    )
+
 
 if __name__ == "__main__":
-    data = read_input(1)
-    solutions = solve(data)
-    print(f"Part 1: {solutions[0]}")
-    print(f"Part 2: {solutions[1]}")
+    data = [line.split() for line in read_input(1)]
+    first_location_codes = [int(x[0]) for x in data]
+    second_location_codes = [int(x[1]) for x in data]
+    solution1, solution2 = solve(first_location_codes, second_location_codes)
+    print(f"Part 1: {solution1}")
+    print(f"Part 2: {solution2}")
--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,16 @@ def read_input(day: int) -> list[str]:
     """
     day_folder = Path(__file__).parent / f"day{day}"
     return (day_folder / "input.txt").read_text().strip().split("\n") 
+
+def read_test_input(day: int) -> list[str]:
+    """
+    Read the test.txt file for the specified day and return its contents split by lines.
+    
+    Args:
+        day (int): The day number (1-25)
+        
+    Returns:
+        list[str]: The contents of the input file split by lines
+    """
+    day_folder = Path(__file__).parent / f"day{day}"
+    return (day_folder / "test.txt").read_text().strip().split("\n") 
